2c76ff3660bb897d5957a7d9bc8b5b6fa4e2a1a2
Frutios <benjamin.tourneur7@gmail.com>
2021-02-16 20:26:57 +0100

    fix test--

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const path = require('path')
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
@@ -19,4 +19,4 @@ app.get('/*', (req, res) => {
 })
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Example app listening on port` + PORT))
