1d018c1d1e6b18fd57066378e3e022adcde3344b
Frutios <benjamin.tourneur7@gmail.com>
2021-02-19 21:36:42 +0100

    tech-challenge terminado

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require(cors);
 const connection = require("./config");
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
+
+app.use(cors());
 
 app.use(express.json());
 
