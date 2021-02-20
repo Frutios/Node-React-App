7ccecd86d9ea8856dde637a620e39d11b107050c
Frutios <benjamin.tourneur7@gmail.com>
2021-02-20 16:15:39 +0100

    tech-challenge terminado

--- config.js
+++ config.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  JAWSDB_URL
+  host: "d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+  , // address of the server
+  user: "kc98mmoqo6d3ml00", // username
+  password: "jwydctc1wdzj5bl4",
+  database: "wgnejndw88ea3vhm",
 });
 
 module.exports = connection;
