eb7125ea61a6eeb64cca20290e6dfec966331a0e
Frutios <benjamin.tourneur7@gmail.com>
2021-02-21 21:11:34 +0100

    test

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -47,7 +47,7 @@ function Form() {
             />
           </div>
           <div className="buttonContainer">
-            <button className="button" onClick={addCrewMate, getCrewMates}>
+            <button className="button" onClick={addCrewMate && getCrewMates} >
               Ajouter
             </button>
             <button className="button" onClick={getCrewMates}>
